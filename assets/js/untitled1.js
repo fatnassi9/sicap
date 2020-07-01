a21ff845a4f455d10064b01c9975719ba6e92e72
fatnassi9 <mouhamed.amine.fatnassi@gmail.com>
2020-07-01 20:38:53 +0100

    Added files

---
+++ assets/js/untitled1.js
@@ -0,0 +1,30 @@
+
+var swiper = new Swiper('.swiper-container', {
+      effect: 'coverflow',
+      grabCursor: true,
+      centeredSlides: true,
+      slidesPerView: 'auto',
+      coverflowEffect: {
+        rotate: 20,
+        stretch: 0,
+        depth: 200,
+        modifier: 1,
+        slideShadows: true,
+      },
+    loop: true,
+      pagination: {
+        el: '.swiper-pagination',
+      },
+    });
+
+
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
